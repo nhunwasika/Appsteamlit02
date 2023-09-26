55342dc1cf6a045a8fe584f1f75f5a10200fead1
nhunwasika <wasikabkk@gmail.com>
2023-09-26 15:22:30 +0700

    .commit

--- Mysteamlit02.py
+++ Mysteamlit02.py
@@ -1,3 +1,24 @@
 import streamlit as st
 
-st.header('Wasika Jittisak')
+import streamlit as st
+st.image('./pic/dd.jpg')
+
+col1,col2=st.columns(2)
+with col1 :
+    st.header('Wasika Jittisak')
+with col2 :
+    st.subheader('สาขาวิทยาการข้อมูล')
+    st.text('คณะวิทยาศาสตร์และเทคโนโลยี')
+
+html_1 = """
+<div style="background-color:#52BE80;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
+<center><h5>สถิติข้อมูลดอกไม้</h5></center>
+</div>
+"""
+st.markdown(html_1, unsafe_allow_html=True)
+st.markdown("")
+
+import pandas as pd
+dt=pd.read_csv('./data/iris.csv')
+st.write(dt.head(10))
+st.button('show bar chart')
